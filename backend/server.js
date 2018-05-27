560339d4f42c5c45098abe3af3303f56b2b18ac7
Tiago Lima <tiagol.ext@vr.com.br>
2018-05-27 19:21:47 -0300

    ajustes no servidor, roteando 404 no express

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
-import express from 'express'
+const express = require('express')
 
 const app = express()
 
-app.use(express.static('./src'))
+app.use(express.static('../frontend/dist'))
+
+app.get('*', function(req, res){
+  res.redirect('/')
+})
 
 app.listen(3005, () => {
   console.log('PokeES6 Running on port 3005')
